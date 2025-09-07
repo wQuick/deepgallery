715177286aed477b70552db7b63238809d120bc9
wQuick <wquick@gmail.com>
2025-09-07 13:13:32 -0300

    Update deepgallery.gpr.py

--- deepgallery.gpr.py
+++ deepgallery.gpr.py
@@ -1,10 +1,12 @@
 # File: images.gpr.py
+from gramps.version import major_version
+
 register(GRAMPLET,
          id="DeepGallery",
          name=_("Deep Gallery"),
          description = _("Gramplet showing all media for active person"),
          version="0.0.1",
-         gramps_target_version="5.2",
+         gramps_target_version=major_version,
          status = STABLE,
          fname="deepgallery.py",
          height = 50,
